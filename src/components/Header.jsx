26841596c5a7382db26a53759d67b500b813c2f9
SevenRGP <sevenrgp@gmail.com>
2024-07-06 19:01:11 +0530

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Logo from '../assets/Logo.svg';
+import { Button } from './index';
+
+const Header = () => {
+  return (
+    <>
+      <header className='w-full flex items-center justify-center'>
+        <nav className='w-full max-w-[1200px] flex items-center justify-between p-2'>
+          <div className='h-7'>
+            <img src={Logo} alt="Logo" className='h-full' />
+          </div>
+
+          <div className='flex'>
+            <Button backShadow={true}>Login</Button>
+          </div>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default Header;
---
+++ src/components/common/Button.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button as HeadlessButton } from '@headlessui/react';
+
+const Button = ({
+  className = '',
+  backShadow = false,
+  children,
+  ...props
+}) => {
+  return (
+    <>
+      <div className='flex relative'>
+        <HeadlessButton className={`min-w-14 px-3 py-1.5 border-[1.5px] border-black rounded-sm bg-white z-10 peer text-sm ${className}`} {...props}>
+          {children}
+        </HeadlessButton>
+        {backShadow && <div className='absolute text-sm min-w-14 px-3 py-1.5 border-[1.5px] border-black rounded-sm text-black bg-black mt-0.5 ml-0.5 peer-hover:mt-1 peer-hover:ml-1 transition-all'>
+          {children}
+        </div>}
+      </div>
+    </>
+  )
+}
+
+export default Button;
